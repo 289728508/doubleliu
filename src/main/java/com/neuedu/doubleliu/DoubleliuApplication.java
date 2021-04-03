97853db3f289b3d76edeadf64d2d55bdafe024d4
289728508 <49913648+289728508@users.noreply.github.com>
2021-04-03 10:59:14 +0800

    第一次提交项目整体

---
+++ src/main/java/com/neuedu/doubleliu/DoubleliuApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu.doubleliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleliuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleliuApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/neuedu/doubleliu/controller/AdminController.java
@@ -0,0 +1,7 @@
+package com.neuedu.doubleliu.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AdminController {
+}
